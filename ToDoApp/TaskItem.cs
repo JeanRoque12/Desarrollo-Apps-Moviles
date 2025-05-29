130e5ac06db5490e0dbcc1fc1e8368cc8ae03259
Jean Roque <jeanroque130403@gmail.com>
2025-05-29 16:48:27 -0400

    Agregando todo app

---
+++ ToDoApp/TaskItem.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+
+namespace ToDoApp.Models
+{
+    public class TaskItem
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ ToDoApp/ToDoApp/ToDoApp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using ToDoApp.Views;
+
+namespace ToDoApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new MainPage());
+        }
+    }
+}
