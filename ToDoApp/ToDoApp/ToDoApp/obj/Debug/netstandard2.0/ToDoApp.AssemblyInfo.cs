95333ea8bfd060efc82101c1122243337eada566
Jean Roque <jeanroque130403@gmail.com>
2025-05-29 22:50:51 -0400

    Agregue el README del ToDoApp

--- ToDoApp/ToDoApp/ToDoApp/obj/Debug/netstandard2.0/ToDoApp.AssemblyInfo.cs
+++ ToDoApp/ToDoApp/ToDoApp/obj/Debug/netstandard2.0/ToDoApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7b2e850ed23dda72eb0f4fcb45fdb580adfcada")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+130e5ac06db5490e0dbcc1fc1e8368cc8ae03259")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
